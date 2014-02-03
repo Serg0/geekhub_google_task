d8170b02364830b66d19bbe70c776c914d14f0be
Serhii Nadolinskyi <serhii.nadolinskyi@gmail.com>
2014-02-04 00:53:13 +0200

    Tasks fragment refactored

--- src/com/geekhub/exam/constants/Constants.java
+++ src/com/geekhub/exam/constants/Constants.java
@@ -2,16 +2,16 @@ package com.geekhub.exam.constants;
 
 public class Constants {
 	
-	public final static String 	GOOGLE_API_KEY 						= "AIzaSyBv5gDSv8eyNiqsLH-Zyn-tMY77IkKxTDc";
-	public final static String 	GOOGLE_PROJECT_NUMBER				= "371652803458";
-	public final static String 	DEFAULT_KEY 						= "@default";
+	public final static String 	GOOGLE_API_KEY 				= "AIzaSyBv5gDSv8eyNiqsLH-Zyn-tMY77IkKxTDc";
+	public final static String 	GOOGLE_PROJECT_NUMBER		= "371652803458";
+	public final static String 	DEFAULT_KEY 				= "@default";
 	
-	public final static String 	TASK_COMPLETED_KEY 					= "completed";
-	public final static String 	TASK_NEEDSACTION_KEY 				= "needsAction";
-	public final static String BROADCAST_ACTION = "com.geekhub.exam.fragments.service_and_broadcast";
-	public final static String BROADCAST_TYPE = "update_data";
-	public final static String BROADCAST_MESSAGE = "update_tasks";
-	public final static Integer SERVICE_TIME_INTERVAL = 15 * 60000;
+	public final static String 	TASK_COMPLETED_KEY 			= "completed";
+	public final static String 	TASK_NEEDSACTION_KEY 		= "needsAction";
+	public final static String 	BROADCAST_ACTION 			= "com.geekhub.exam.fragments.service_and_broadcast";
+	public final static String 	BROADCAST_TYPE 				= "update_data";
+	public final static String 	BROADCAST_MESSAGE 			= "update_tasks";
+	public final static Integer SERVICE_TIME_INTERVAL 		= 15 * 60000;
 	
 	
 	
